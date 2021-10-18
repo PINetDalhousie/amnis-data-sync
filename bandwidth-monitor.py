bb76e3e400821dddc7324f45cbe08d463efdc3a8
MOHAMMAD MONZURUL AMIN IFATH <monzurulamin10@gmail.com>
2021-10-18 10:28:02 -0300

    Kraft updated

--- bandwidth-monitor.py
+++ bandwidth-monitor.py
@@ -7,13 +7,35 @@ import sys
 
 interval = 5
 
-os.system("sudo mkdir logs/bandwidth/")
-switches = int(sys.argv[1]) #get the passed argument
+#get and process the passed arguments
+inputDetails = sys.argv[1]
+firstSplit = inputDetails.split('mSize:')
+switches = int(firstSplit[0])
+secondSplit = firstSplit[1].split('mRate:')
+mSizeString = secondSplit[0]
+thirdSplit = secondSplit[1].split('nTopics:')
+mRate = float(thirdSplit[0])
+fourthSplit = thirdSplit[1].split('replication:')
+nTopics = int(fourthSplit[0])
+# replication = int(fourthSplit[1])
+fifthSplit = fourthSplit[1].split('nZk:')
+replication = int(fifthSplit[0])
+nZk = int(fifthSplit[1])
+
+# overwrite the directory if exists
+if nZk == 0:
+	os.system("sudo rm -rf logs/kraft/bandwidth/"+"nodes:" +str(switches)+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/"+"; sudo mkdir logs/kraft/bandwidth/"+"nodes:" +str(switches)+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/")
+else:
+	os.system("sudo rm -rf logs/kafka/bandwidth/"+"nodes:" +str(switches)+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/"+"; sudo mkdir logs/kafka/bandwidth/"+"nodes:" +str(switches)+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/")
+    
 
 while True:
 
 	for i in range(switches):
-		bandwidthLog = open("logs/bandwidth/bandwidth-log" + str(i+1) + ".txt", "a")
+		if nZk == 0:        
+			bandwidthLog = open("logs/kraft/bandwidth/"+"nodes:" +str(switches)+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/bandwidth-log" + str(i+1) + ".txt", "a")
+		else:
+			bandwidthLog = open("logs/kafka/bandwidth/"+"nodes:" +str(switches)+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/bandwidth-log" + str(i+1) + ".txt", "a")
 
 		statsProcess = subprocess.Popen("sudo ovs-ofctl dump-ports s"+str(i+1), shell=True, stdout=subprocess.PIPE)
 		stdout = statsProcess.communicate()[0]
--- emuLoad.py
+++ emuLoad.py
@@ -7,9 +7,11 @@ from random import seed, randint
 
 import time
 import os
+import sys
 
+leaderReplicaList = []
 
-def spawnProducers(net, mSizeString, mRate, tClassString, nTopics):
+def spawnProducers(net, mSizeString, mRate, tClassString, nTopics, nZk):
 
 	tClasses = tClassString.split(',')
 	#print("Traffic classes: " + str(tClasses))
@@ -37,20 +39,20 @@ def spawnProducers(net, mSizeString, mRate, tClassString, nTopics):
 
 	for nodeList in nodeClassification.values():
 		for node in nodeList:
-			node.popen("python3 producer.py "+str(node)+" "+tClasses[i]+" "+mSizeString+" "+str(mRate)+" "+str(nTopics)+" &", shell=True)
+			node.popen("python3 producer.py "+str(node)+" "+tClasses[i]+" "+mSizeString+" "+str(mRate)+" "+str(nTopics)+" "+str(nZk)+" &", shell=True)
 
 		i += 1
 
 
-def spawnConsumers(net, nTopics, rate):
+def spawnConsumers(net, nTopics, rate, nZk):
 	#h2.cmd("python3 kafka-python-consumer.py > consumed-data.txt", shell=True)
 	#print("Data consumed")
 
 	for node in net.hosts:
-		node.popen("python3 consumer.py "+str(node.name)+" "+str(nTopics)+" "+str(rate)+" &", shell=True)
+		node.popen("python3 consumer.py "+str(node.name)+" "+str(nTopics)+" "+str(rate)+" "+str(nZk)+" &", shell=True)
 
 
-def runLoad(net, nTopics, replication, mSizeString, mRate, tClassString, consumerRate, duration, topicWaitTime=100):
+def runLoad(net, nTopics, replication, mSizeString, mRate, tClassString, consumerRate, duration, nZk, topicWaitTime=300):
 
 	print("Start workload")
 
@@ -64,7 +66,14 @@ def runLoad(net, nTopics, replication, mSizeString, mRate, tClassString, consume
 	for i in range(nTopics):
 		issuingID = randint(0, nHosts-1)
 		issuingNode = net.hosts[issuingID]
+		kraftPort = 2181        
 
+# 		if nZk == 0:        
+# 			issuingNode.popen("kafka/bin/kafka-topics.sh --create --bootstrap-server 10.0.0."+str(issuingID+1)+":"+str(kraftPort+issuingID)+ " --replication-factor "+str(replication)+" --partitions 1 --topic topic-"+str(i)+" &", shell=True)
+# 			print("Creating kraft topic "+str(i)+" at broker "+str(issuingID+1))            
+# 		else:        
+# 			issuingNode.popen("kafka/bin/kafka-topics.sh --create --bootstrap-server 10.0.0."+str(issuingID+1)+":9092 --replication-factor "+str(replication)+" --partitions 1 --topic topic-"+str(i)+" &", shell=True)            
+# 			print("Creating kafka topic-"+str(i)+" at broker "+str(issuingID+1))
 		issuingNode.popen("kafka/bin/kafka-topics.sh --create --bootstrap-server 10.0.0."+str(issuingID+1)+":9092 --replication-factor "+str(replication)+" --partitions 1 --topic topic-"+str(i)+" &", shell=True)
 		print("Creating topic "+str(i)+" at broker "+str(issuingID+1))
 		topicNodes.append(issuingNode)
@@ -72,6 +81,51 @@ def runLoad(net, nTopics, replication, mSizeString, mRate, tClassString, consume
 	topicWait = True
 	startTime = time.time()
 	totalTime = 0
+	topicNumber = 0    
+    
+    # printing the list using loop
+	for x in range(len(topicNodes)):
+	    print (topicNodes[x])
+    
+# 	for host in topicNodes:
+# 	    while topicWait:
+# 	        if topicNumber < nTopics:
+# 	            print("Checking Topic Creation for Host " + str(host.IP()) + "...")
+# 	            out = host.cmd("kafka/bin/kafka-topics.sh --list --bootstrap-server " + str(host.IP()) + ":9092", shell=True)
+                
+# 	            topicDescription = host.cmd("kafka/bin/kafka-topics.sh --bootstrap-server " + str(host.IP()) + ":9092 --describe --topic topic-"+str(topicNumber), shell=True)
+# 	            print(topicDescription) 
+# 	            if topicDescription.find('Leader:') != -1:
+#                     # parse and get the leader id from topic description                
+# 	                leaderSplit = topicDescription.split('Leader: ')   
+#     # 	            print("leaderSplit: " + str(leaderSplit))
+#                     # using list comprehension
+#     # 	            listToStr = ' '.join(map(str, leaderSplit))
+#     # 	            if listToStr.find('Replicas:') != -1:
+# 	                leaderNumberSplit = leaderSplit[1].split('Replicas:')           
+# 	                leaderNumber = int(leaderNumberSplit[0])
+# 	                leaderReplicaList.append(leaderNumber)
+# 	                print("leaderNumber: " + str(leaderNumber))
+# 	                topicNumber = topicNumber+1
+            
+    
+# 	            stopTime = time.time()
+# 	            totalTime = stopTime - startTime
+# 	            if "topic-" in out:
+# 	                topicWait = False
+# 	                print(out)
+# 	            elif(totalTime > topicWaitTime):
+# 	                print("ERROR: Timed out waiting for topics to be created")
+# 	                sys.exit(1)
+# 	            else:
+# 	                time.sleep(10)
+# 	        else:
+# 	            break
+# 	    topicWait = True
+# # 	    topicNumber = 0
+	    
+# 	print("Successfully Created Topics in " + str(totalTime) + " seconds")
+
 	for host in topicNodes:
 	    while topicWait:
 	        print("Checking Topic Creation for Host " + str(host.IP()) + "...")
@@ -90,12 +144,22 @@ def runLoad(net, nTopics, replication, mSizeString, mRate, tClassString, consume
 	    
 	print("Successfully Created Topics in " + str(totalTime) + " seconds")
 	
+    # saving leader replica list in a file for convenience
+# 	if nZk == 0:
+# 	    folderPath = "logs/kraft/bandwidth/"
+# 	else:
+# 	    folderPath = "logs/kafka/bandwidth/"
+# 	leaderFile = open(folderPath+"nodes:" +str(len(net.hosts))+ "_mSize:"+ mSizeString+ "_mRate:"+ str(mRate)+ "_topics:"+str(nTopics) +"_replication:"+str(replication)+"/leader-list.txt", "w")    
+# 	for element in leaderReplicaList:
+# 	    leaderFile.write(str(element) + "\n")
+# 	leaderFile.close()
+# 	print("Leader replica list updated in file")
 
-	spawnProducers(net, mSizeString, mRate, tClassString, nTopics)
+	spawnProducers(net, mSizeString, mRate, tClassString, nTopics, nZk)
 	time.sleep(1)
 	print("Producers created")
 
-	spawnConsumers(net, nTopics, consumerRate)
+	spawnConsumers(net, nTopics, consumerRate, nZk)
 	time.sleep(1)
 	print("Consumers created")
 
