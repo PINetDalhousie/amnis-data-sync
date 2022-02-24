bd7b08cd7c15be3c2b9e47baa333ea609d3f357a
Michael Basnak <michael.basnak@gd-ms.ca>
2022-02-24 18:07:32 -0400

    small fixes for cpu-mem-monitor and latencyPlotScript

--- cpu-mem-monitor.py
+++ cpu-mem-monitor.py
@@ -9,10 +9,10 @@ interval = 0.5
 
 test = str(sys.argv[1])
 
-if not os.path.isdir:
-	os.system("sudo mkdir logs/cpu-mem/")
+if not os.path.isdir("logs/cpu-mem/"):
+	os.system("sudo mkdir -p logs/cpu-mem/")
 
-
+print("Monitoring CPU and memory. Press Ctrl+C to stop.")
 while True:
 	os.system("top -b -n 1 | head -n 3 | tail -n 1 | awk '{print $2}' >> logs/cpu-mem/" + test + "-cpu.log")
 	os.system("top -b -n 1 | head -n 4 | tail -n 1 | awk '{print $8}' >> logs/cpu-mem/" + test + "-mem.log")
