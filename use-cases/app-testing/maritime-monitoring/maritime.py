d49af1149b57c2d4e333107db967dc4e944e39c2
ifath <monzurulamin10@gmail.com>
2022-08-10 12:19:48 -0300

    Setup MySQL connection for Maritime application

--- use-cases/app-testing/maritime-monitoring/maritime.py
+++ use-cases/app-testing/maritime-monitoring/maritime.py
@@ -1,8 +1,12 @@
+# This file is going to be our maritime application. At this point, this file reads AIS data from a kafka topic,
+# does some processing, then stores the result in a kafka topic with a particular json format.
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
-
+from time import sleep
+from json import dumps
+from kafka import KafkaProducer
 import sys
 import logging
 
@@ -24,65 +28,118 @@ try:
     logging.info("output: "+sparkOutputTo)
 
     
+    # Some basic spark setup
     spark = SparkSession.builder \
-        .appName("Maritime Spark") \
-        .getOrCreate()
+            .appName("Spark Structured Streaming for Maritime") \
+            .getOrCreate()
 
     spark.sparkContext.setLogLevel('ERROR')
 
-    # vessel = spark\
-    #     .readStream\
-    #     .format('socket')\
-    #     .option('host', host)\
-    #     .option('port', port)\
-    #     .load()\
-    #     .selectExpr("CAST(value AS STRING)")
+    # Now, we read in the AIS data from the kafka topic
 
-    # reading from Kafka source
-    vesselDf = spark.readStream\
-                .format("kafka")\
-                .option("kafka.bootstrap.servers", kafkaNode)\
-                .option("subscribe", sparkInputFrom)\
-                .option("startingOffsets", "earliest")\
-                .load()\
-                .selectExpr("CAST(value AS STRING)")
+    vessel = spark\
+            .readStream\
+            .format('kafka')\
+            .option('kafka.bootstrap.servers', kafkaNode)\
+            .option('subscribe', sparkInputFrom)\
+            .option("startingOffsets", "earliest")\
+            .load()\
+            .selectExpr("CAST(value AS STRING)")
 
-    vesselSchema = StructType( [StructField('MMSI', IntegerType()),\
-        StructField('BaseDateTime', TimestampType()),\
-        StructField('LAT', DoubleType()),\
-        StructField('LON', DoubleType())])
+    # Since the data we read is in json format, we extract the fields we want and use those to make new columns
 
-    # parsing dataframe according to provided schema
-    def parse_data_from_kafka_message(sdf, schema):
-        from pyspark.sql.functions import split
-        assert sdf.isStreaming == True, "DataFrame doesn't receive treaming data"
-        col = split(sdf['value'], ',') #split attributes to nested array in one Column
-        #now expand col to multiple top-level columns
-        for idx, field in enumerate(schema): 
-            sdf = sdf.withColumn(field.name, col.getItem(idx).cast(field.dataType))
-        return sdf.select([field.name for field in schema])
+    vessel = vessel.select(json_tuple(col("value"),"mmsi", "lat", "lon")) \
+            .toDF("mmsi", "lat", "lon")
 
-    vessel = parse_data_from_kafka_message(vesselDf, vesselSchema)
-    query = vessel.groupBy(window("BaseDateTime","1 hour"),"MMSI")\
-        .agg( (count("MMSI").alias("Number of Updates")))\
-            .orderBy("window.start")
+    
+    # Here we determine the largest and smallest reported longitude of each ship
+    
+    query = vessel.groupBy("mmsi")\
+            .agg( max("lon"), min("lon") )
 
-    # writing processed dataframe to Kafka sink
-    output = query\
+
+    # Here we get the dataframe columns, which we will use in creating our json string
+
+    columns = []
+
+    for column in query.columns:
+        columns.append(column)
+
+    # Convert the result of our query to json format
+
+    query = query.select( to_json( struct("*")).alias("value") )
+
+    
+    # Here we create our required json schema
+
+    def construct_json_string(columns):
+
+        jsonString = '{"schema":{"type":"struct","optional":false,"version":1,"fields":['
+
+        i = 0
+
+        for column in columns:
+
+            jsonString += '{"field":"' + column + '","type":"string","optional":true}'
+
+            if i < len(columns) - 1:
+                
+                jsonString += ','
+                i+=1
+        
+        jsonString += ']},"payload":'
+
+        return jsonString
+
+    # Getting our json schema string
+
+    jsonString = construct_json_string(columns)
+    
+    i = 0
+
+    # Here we send each of our dataframe row to the output kafka topic. Each row is first formatted as follows
+    #
+    # Schema followed by the row data
+    #
+    # Each formatted row is then sent to the kafka topic
+
+    class RowPrinter:
+
+        # This part is just left as the default
+
+        def open(self, partition_id, epoch_id):
+            return True
+
+        # This is where we format and send each row to the kafka topic
+
+        def process(self, row):
+
+            global i
+            global jsonString
+
+            if i == 0:
+                producer = KafkaProducer(bootstrap_servers=[kafkaNode])#,\
+                        # value_serializer=lambda x:\
+                        # dumps(x).encode('utf-8'))
+                i+= 10
+                jsonString += str(row[0]) + "}"
+                jsonByte = bytes(jsonString, 'utf-8')
+
+                producer.send(sparkOutputTo, jsonByte)
+        
+        # This part is also left as the default
+
+        def close(self, error):
+            print("Closed with error: %s" % str(error))
+
+    # Sending the dataframe to the kafka topic row by row, in the desired json format
+
+    output =query\
         .writeStream\
-        .format("kafka")\
         .outputMode("complete")\
-        .option("kafka.bootstrap.servers", kafkaNode)\
-        .option("topic", sparkOutputTo)\
-        .option("checkpointLocation", "logs/output/maritimeCheckpoint")\
-        .start()
-
-    # writing the dataframe in a csv file
-    # output = query.writeStream \
-    # .format("csv") \
-    # .option("path", "/tmp/maritime_output") \
-    # .option("checkpointLocation", "/tmp/maritime_checkpoint") \
-    # .start() 
+        .foreach(RowPrinter())\
+        .start()\
+        .awaitTermination()
     
     output.awaitTermination(30)
     output.stop()
